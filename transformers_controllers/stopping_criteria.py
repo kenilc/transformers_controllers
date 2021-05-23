853081d98c5e246335a6a01d20d3477b8c2b9d48
Kenil Cheng <kenilc@gmail.com>
2021-05-23 11:50:51 +0000

    Added SuffixCriteria

---
+++ transformers_controllers/stopping_criteria.py
@@ -0,0 +1,25 @@
+from typing import List, Optional
+
+from pygtrie import Trie
+import torch
+from transformers import StoppingCriteria
+
+class SuffixCriteria(StoppingCriteria):
+    '''
+    This class can be used to stop generation whenever one of the suffix sequences
+    matches the suffix of the generated tokens.
+
+    Args:
+        suffixes_ids: list[list[int]]
+            List of suffix sequence tokens in the vocabulary
+    '''
+
+    def __init__(self, suffixes_ids: List[List[int]]):
+        self.reversed_suffixes_ids = Trie()
+        for suffix_ids in suffixes_ids:
+            self.reversed_suffixes_ids[suffix_ids[::-1]] = True
+
+    def __call__(self, input_ids: torch.LongTensor, score: torch.FloatTensor, **kwargs) -> bool:
+        reversed_tokens = input_ids[0, :].flip(0).tolist()
+        return self.reversed_suffixes_ids.shortest_prefix(reversed_tokens)[0] is not None
+
